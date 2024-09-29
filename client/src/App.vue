063fedc6f5d34f9eb50182d407f00ce771ea9735
iuroc <m@iuroc.com>
2024-09-30 01:57:15 +0800

    ....

---
+++ client/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>{{ message }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('Hello World')
+</script>
--- server/main.go
+++ server/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("你好呀1231212")
 }
