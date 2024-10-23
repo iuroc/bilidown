f53d19b3710d6c7b8c23799647917df497444b3c
iuroc <m@iuroc.com>
2024-10-24 01:01:15 +0800

    ...

--- server/router/router.go
+++ server/router/router.go
@@ -4,8 +4,6 @@ import "net/http"
 
 func API() *http.ServeMux {
 	router := http.NewServeMux()
-	router.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("6666"))
-	})
+	
 	return router
 }
