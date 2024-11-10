33bb558a7f7ae8b85f570e28d6ba19809fee1153
iuroc <m@iuroc.com>
2024-11-10 19:32:41 +0800

    ...

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         host: '0.0.0.0'
     },
     build: {
-        outDir: '../server/static',
+        outDir: '../static',
         emptyOutDir: true,
     },
     css: {
