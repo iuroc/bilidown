257fd6346366c0d93b9e7ace4398871847144365
iuroc <m@iuroc.com>
2025-05-21 16:39:28 +0800

    ...

--- dist/util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sum = sum;
-function sum(a, b) {
-    return a + b;
-}
--- src/main.ts
+++
@@ -1,3 +0,0 @@
-import('gede-book-api').then(async ({ Book }) => {
-    console.log((await Book.getCategories()).map(i => i.name))
-})
