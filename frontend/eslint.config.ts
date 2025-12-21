ceb9117c6cff81edfda542ae6859e64c736fe557
iuroc <m@iuroc.com>
2025-12-21 21:10:50 +0800

    chore: 更新 ESLint 配置以忽略特定目录，调整 tsconfig.json 和 Vite 配置

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,10 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
+    globalIgnores(['dist/', 'node_modules/']),
     {
         files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
         plugins: {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     base: './',
     resolve: {
         alias: {
-            '@': '/src',
-        },
+            '@': '/src'
+        }
     }
 })
